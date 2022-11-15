64650821304711f1963aca4853e34c80a9d19344
Aditya Bhardwaj <abd.aditya10@outlook.com>
2022-11-15 23:30:07 +0530

    .

---
+++ Lab 2/2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int checkPrime(int num)
+{
+    if (num < 2)
+    {
+        return 0;
+    }
+    else
+    {
+        int x = num / 2;
+        for (int i = 2; i < x; i++)
+        {
+            if (num % i == 0)
+            {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+int main()
+{
+    int b, a = 1;
+    cout << "Enter number: ";
+    cin >> b;
+    for (int i = a; i <= b; i++)
+    {
+        if (checkPrime(i))
+            cout << i << " ";
+    }
+    return 0;
+}
